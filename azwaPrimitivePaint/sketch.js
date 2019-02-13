07bd7295fced4cd620f2308988305be971646a23
azwaalam <azwaalam1@gmail.com>
2019-02-13 10:50:53 -0600

    state var demo

--- azwaPrimitivePaint/sketch.js
+++ azwaPrimitivePaint/sketch.js
@@ -10,16 +10,18 @@ function setup() {
   background(51);
 }
 
-function keyTyped() {
+function keyPressed() {
   if (key === 'a') {
     rect(mouseX, mouseY, 20, 20);
-  
+  }
   if (key === 's') {
     ellipse(mouseX, mouseY, 55, 55);
+  }
+  if (key === 'd') {
+    quad(mouseX+38, mouseY+31, mouseX+86, mouseY+20, mouseX+69, mouseY+63, 30, mouseX+76);
   }
 }
 function draw() {
   background(51);
-  keyTyped();
+  keyPressed();
 }
-  
