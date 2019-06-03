995c8af9311da61b9d48309ec242e0e0b03e9bb7
azwaalam <azwaalam1@gmail.com>
2019-06-03 15:41:34 -0600

    Update sketch.js

--- azwaMajorProject/sketch.js
+++ azwaMajorProject/sketch.js
@@ -5,45 +5,65 @@
 
 let x;
 let y;
-let shapeX = 0;
+let shapeX = 20;
 let shapeY;
 let xOff = 0;
 let curHeight;
 let offset; 
 let amountToChange = 0.01;
 let originalOffSet; 
+let offsets; 
+const STEP_AMOUNT = 30; 
+ 
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   originalOffSet = random(10); 
   curHeight = height/2; 
 
+  offsets = [];
+  for(let a = 0; a < 361; a ++){
+    offsets.push(random(100)); 
+  }
+  //noLoop();
+
 }
 
 function weirdCircle(x, y) { //draws a circle with random angles
   noFill();
-  stroke(map(shapeX, 0, width, 100, 153), map(shapeX, 0, width, 100, 204), map(shapeX, 0, width, 150, 255));
+  stroke(map(shapeX, 0, width, 100, 153), map(shapeX, 0, width, 100, 204), map(shapeX, 0, width, 150, 255), 200);
   push();
   translate(x, y);
   beginShape();
   offset = originalOffSet; 
   let origX, origY;
-  for (let i = 0; i <= 360; i += 2) {
-    let r = 30 + noise(offset) * 150; //gives a controlled random line
+  let secondX, secondY; 
+
+
+  for (let i = 0; i <= 360; i += STEP_AMOUNT) {
+    let r = 30 + noise(offsets[i]) * 150 ; //gives a controlled random line
     x = r * cos(radians(i));
     y = r * sin(radians(i));
     if (i === 0) {
       origX = x;
       origY = y;
     }
-    else if (i === 360) {
-      x = origX;
-      y = origY;
+    else if(i === STEP_AMOUNT){
+      secondX = x;
+      secondY = y; 
     }
-    offset += 0.05; //0.1
-    vertex(x, y);
+    else if(i === 360) {
+      curveVertex(origX, origY);
+      curveVertex(secondX, secondY);
+    }
+
+    else{
+      offsets[i] += 0.01; 
+      curveVertex(x, y);
+    }
+
   }
-  endShape();
+  endShape(CLOSE);
   pop();
   originalOffSet += amountToChange; 
 }
